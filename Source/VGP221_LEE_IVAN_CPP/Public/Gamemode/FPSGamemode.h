e0637a340cbeda82b3dbf916a1d7ce9660ecacff
ilee <lee.4an@gmail.com>
2024-01-10 20:47:34 -0500

    Added Gamemode

---
+++ Source/VGP221_LEE_IVAN_CPP/Public/Gamemode/FPSGamemode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSGamemode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VGP221_LEE_IVAN_CPP_API AFPSGamemode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
